ebb0b29cf3906c55033008449ef336a098434b67
root <root@8a8882db4970>
2020-09-06 18:15:08 +0000

    yeet

--- src/_03_smurf/SmurfRunner.java
+++ src/_03_smurf/SmurfRunner.java
@@ -2,7 +2,7 @@ package _03_smurf;
 
 public class SmurfRunner {
 public static void main(String[] args) {
-	Smurf handy = new Smurf("ur mum");
+	Smurf handy = new Smurf("Handy");
 	System.out.println(handy.getName());
 }
 }
