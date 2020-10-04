4cc8603d98e8d72d7fc6f1571308ad1761190fe6
root <root@ca92a6505a56>
2020-10-04 18:26:02 +0000

    yeet

--- src/_06_minion/Minion.java
+++ src/_06_minion/Minion.java
@@ -1,8 +1,27 @@
 package _06_minion;
 
 public class Minion {
-	private String name; 
-		private int eyes; 
-		private String color; 
-		private String master;
+	private String name;
+	private int eyes;
+	private String color;
+	private String master;
+
+	
+	
+	public String getName() {
+		return this.name;
+	}
+
+	public int getEyes() {
+		return this.eyes;
+	}
+
+	public String getColor() {
+		return this.color;
+	}
+
+	public String getMaster() {
+		return this.master;
+	}
+
 }
