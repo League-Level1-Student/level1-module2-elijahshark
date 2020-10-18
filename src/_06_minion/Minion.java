b682b4d546cf8c1a239e7c53d09c91a153d6f1e4
root <root@c989f8f31a84>
2020-10-18 18:05:40 +0000

    Yeet

--- src/_06_minion/Minion.java
+++ src/_06_minion/Minion.java
@@ -5,9 +5,15 @@ public class Minion {
 	private int eyes;
 	private String color;
 	private String master;
-	minion()
-	
-	
+	Minion(String name, int eyes, String color,String master){
+	this.name = name;
+	this.eyes = eyes;
+	this.color = color;
+	this.master = master;	
+	}	
+		
+		
+		
 	public String getName() {
 		return this.name;
 	}
